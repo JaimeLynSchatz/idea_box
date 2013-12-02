1d87ced2f20baee4e8e6e3312c4908c5559dc927
JaimeLynSchatz <jaimelynschatz@gmail.com>
2013-12-02 11:47:36 -0800

    create YAML db to store ideas

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+require './idea'
+
 class IdeaBoxApp < Sinatra::Base
   not_found do
     erb :error
@@ -13,7 +15,9 @@ class IdeaBoxApp < Sinatra::Base
 
   post '/' do
     # 1. Create an idea based on the form parameters submitted
+    idea = Idea.new
     # 2. Store it
+    idea.save
     # 3. Send us back to the index page to see all the ideas
     "Creating an IDEA!"
   end
--- idea.rb
+++ idea.rb
@@ -1,2 +1,14 @@
+require 'yaml/store'
+
 class Idea
+  def save
+    database.transaction do |db|
+      db['ideas'] ||= []
+      db['ideas'] << {title: 'diet', description: 'pizza all the time'}
+    end
+  end
+
+  def database
+    @database ||= YAML::Store.new "ideabox"
+  end
 end
