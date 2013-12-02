28f3db90bb8ea74867641ce43839f9f076d9481f
JaimeLynSchatz <jaimelynschatz@gmail.com>
2013-12-02 10:55:27 -0800

    enhance error message screen to include debugging info and create post method for creating a new idea

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 class IdeaBoxApp < Sinatra::Base
+  not_found do
+    erb :error
+  end
+
   configure :development do
     register Sinatra::Reloader
   end
-  
+
   get '/' do
     erb :index
   end
+
+  post '/' do
+    "Creating an IDEA!"
+  end
+  
 end
