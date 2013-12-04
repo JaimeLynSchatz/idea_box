50f8eb952301d73f79cd5f9b01fd225c1f74963a
JaimeLynSchatz <jaimelynschatz@gmail.com>
2013-12-04 12:50:04 -0800

    refactor the POST on root method

--- app.rb
+++ app.rb
@@ -35,7 +35,7 @@ class IdeaBoxApp < Sinatra::Base
   end
 
   post '/' do
-    idea = Idea.new(title: params['idea_title'], description: params['idea_description'])
+    idea = Idea.new(params[:idea])
     idea.save
     redirect '/'
   end
