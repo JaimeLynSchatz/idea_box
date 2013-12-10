4d400860a143e151f359d5d26979ef10e86b50e7
JaimeLynSchatz <jaimelynschatz@gmail.com>
2013-12-10 09:57:59 -0800

    add ranking to ideas

--- lib/idea_box/idea.rb
+++ lib/idea_box/idea.rb
@@ -1,10 +1,15 @@
 class Idea
 
-  attr_reader :title, :description
+  attr_reader :title, :description, :rank
 
   def initialize(attributes = {})
     @title = attributes["title"]
     @description = attributes["description"]
+    @rank = attributes["rank"] || 0
+  end
+
+  def save
+    IdeaStore.create("title" => title, "description" => description, "rank" => rank)
   end
 
 end
